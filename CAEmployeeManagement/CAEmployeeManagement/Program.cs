a8e7230459f2597af995a049dbf994786fdc1725
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-11-21 19:08:36 +0400

    Initial configurations are completed

---
+++ CAEmployeeManagement/CAEmployeeManagement/Program.cs
@@ -0,0 +1,23 @@
+namespace CAEmployeeManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services
+                .AddMvc()
+                .AddRazorRuntimeCompilation();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=home}/{action=index}");
+
+            app.Run();
+        }
+    }
+}
