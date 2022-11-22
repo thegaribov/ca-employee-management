d47ca53b0c7dc417c5c9847423845278dee4accb
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-11-22 21:51:28 +0400

    FIN code attribute validation added

---
+++ CAEmployeeManagement/CAEmployeeManagement/Attributes/Employee/FINValidationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace CAEmployeeManagement.Attributes.Employee
+{
+    public class FINValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            ArgumentNullException.ThrowIfNull(value, $"{nameof(RequiredAttribute)} requred");
+
+            var fin = value.ToString();
+            string pattern = @"^([A-Z0-9]{7}$)";
+            return Regex.IsMatch(fin, pattern);
+        }
+    }
+}
--- CAEmployeeManagement/CAEmployeeManagement/ViewModels/Employee/AddViewModel.cs
+++ CAEmployeeManagement/CAEmployeeManagement/ViewModels/Employee/AddViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace CAEmployeeManagement.ViewModels.Employee
+﻿using CAEmployeeManagement.Attributes.Employee;
+using System.ComponentModel.DataAnnotations;
+
+namespace CAEmployeeManagement.ViewModels.Employee
 {
     public class AddViewModel : BaseViewModel
     {
+        [Required]
+        [FINValidation]
         public string PIN { get; set; }
         public string Email { get; set; }
     }
